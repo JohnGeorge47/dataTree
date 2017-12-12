29c19436a7cf966a7f903bdebf102c72e17dbac5
John <john.george@mountblue.io>
2017-12-12 16:10:24 +0530

    Changes to the public folder

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var tree=$('.tree');
     var j=0;
+    var k=0;
     $.ajax({
        type: "GET",
        url :"moi.json",
@@ -11,11 +12,19 @@ $(document).ready(function(){
          for(var props in data)
          {
             var id='district'+j;
-            tree.append("<li><span class='collapsable'>"+props+"</span><ul id='"+id+"'>"+"<ul></li>" );
+            tree.append("<li>"+props+"<ul id='"+id+"'>"+"<ul></li>" );
             for(i=0;i<data[props].length;i++){
                 for(var key in data[props][i])
                 {
-                    $(`#${id}`).append("<li><span class='collapsable'>"+key+"</span><ul<ul></li>");
+                    id2='newId'+k;
+
+                    $(`#${id}`).append("<li>"+key+"<ul id='"+id2+"''>"+"</ul></li>");
+                    for(var next in data[props][i][key]){
+
+                            $(`#${id2}`).append("<li>"+next+"</li>");
+
+                    }
+                    k++;
                 }
             }
             j++;
@@ -26,18 +35,14 @@ $(document).ready(function(){
        },
        dataType:"json",
     });
-       $(".collapsable").click(function () {
+       $(".tree").click(function () {
 
         $(this).parent().children().toggle();
-        $(this).toggle();
+
 
     });
 
-$(".collapsable").each(function(){
 
-        $(this).parent().children().toggle();
-        $(this).toggle();
-});
 
 
 });
