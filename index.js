b21c278d24d1bc58d016727fbfebac9eab56e931
John <john.george@mountblue.io>
2017-12-12 10:22:52 +0530

    Add change to the API structure

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var csv =require('csv-parser');
 var fs=require('fs')
 var obj = {
   "district_name": [],
-  "block_name": []
+  "block_name": [],
+  "moi":[]
 };
 fs.createReadStream('primaryschool.csv')
   .pipe(csv()).on('data', function (data) {
     obj.district_name.push(data.district_name);
     obj.block_name.push(data.block_name);
+    obj.moi.push(data.moi);
   })
 
   .on('end', function () {
@@ -18,13 +20,22 @@ fs.createReadStream('primaryschool.csv')
         }
 
    });
-   var blockname=[]
+   var blockname=[];
     obj.block_name.map(function(dist){
         if(blockname.indexOf(dist)==-1){
             blockname.push(dist);
         }
 
    });
+    var medium=[];
+    obj.moi.map(function(med){
+        if(med!=''){
+        if(medium.indexOf(med)==-1){
+            medium.push(med);
+        }
+    }
+
+    });
 
 
  const average = obj.district_name.reduce((total, amount, index, array) => {
@@ -33,6 +44,12 @@ fs.createReadStream('primaryschool.csv')
       total.push(n);
       return total
     }, []);
+
+var blockno = obj.block_name.reduce(function (total, mo) {
+      total[mo] = (total[mo] || 0) + 1;
+      return total;
+  },{});
+
     var arr=[];
     var brr=[];
     var obj1={};
@@ -44,6 +61,7 @@ fs.createReadStream('primaryschool.csv')
         for(var prop in dist){
           if(obj1.hasOwnProperty(prop))
           {
+
             if(obj1[prop].indexOf(dist[prop])==-1){
               {
                 obj1[prop].push(dist[prop]);
@@ -53,6 +71,17 @@ fs.createReadStream('primaryschool.csv')
         }
 
     });
+    console.log(blockno);
+    for(var prop in obj1){
+        for(i=0;i<obj1[prop].length;i++){
+            var obj3={};
+            if(blockno.hasOwnProperty(obj1[prop][i]))
+            {
+                obj3[obj1[prop][i]]=blockno[obj1[prop][i]];
+                obj1[prop][i]=obj3;
+            }
+        }
+    }
     console.log(obj1);
 
   });
